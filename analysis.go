17abd9ad043c3a879124220345c1f98cc35e5bfd
OctopusLian <zoctopus@qq.com>
2021-11-05 12:43:25 +0800

    完善

--- analysis.go
+++ analysis.go
@@ -1,36 +1,232 @@
-package main
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="ChangeListManager">
+    <list default="true" id="28dbcba6-7f04-48da-b805-a83f6fd71034" name="Default" comment="" />
+    <option name="EXCLUDED_CONVERTED_TO_IGNORED" value="true" />
+    <option name="TRACKING_ENABLED" value="true" />
+    <option name="SHOW_DIALOG" value="false" />
+    <option name="HIGHLIGHT_CONFLICTS" value="true" />
+    <option name="HIGHLIGHT_NON_ACTIVE_CHANGELIST" value="false" />
+    <option name="LAST_RESOLUTION" value="IGNORE" />
+  </component>
+  <component name="FileEditorManager">
+    <leaf>
+      <file leaf-file-name="analysis.go" pinned="false" current-in-tab="true">
+        <entry file="file://$PROJECT_DIR$/analysis.go">
+          <provider selected="true" editor-type-id="text-editor">
+            <state relative-caret-position="-5594">
+              <caret line="5" column="10" lean-forward="false" selection-start-line="5" selection-start-column="10" selection-end-line="5" selection-end-column="10" />
+              <folding />
+            </state>
+          </provider>
+        </entry>
+      </file>
+    </leaf>
+  </component>
+  <component name="FileTemplateManagerImpl">
+    <option name="RECENT_TEMPLATES">
+      <list>
+        <option value="Go Application" />
+      </list>
+    </option>
+  </component>
+  <component name="FindInProjectRecents">
+    <findStrings>
+      <find>redis</find>
+      <find>log.</find>
+    </findStrings>
+  </component>
+  <component name="GOROOT" path="/usr/local/Cellar/go/1.9.2/libexec" />
+  <component name="IdeDocumentHistory">
+    <option name="CHANGED_PATHS">
+      <list>
+        <option value="$PROJECT_DIR$/analysis.go" />
+      </list>
+    </option>
+  </component>
+  <component name="JsBuildToolGruntFileManager" detection-done="true" sorting="DEFINITION_ORDER" />
+  <component name="JsBuildToolPackageJson" detection-done="true" sorting="DEFINITION_ORDER" />
+  <component name="JsGulpfileManager">
+    <detection-done>true</detection-done>
+    <sorting>DEFINITION_ORDER</sorting>
+  </component>
+  <component name="ProjectFrameBounds">
+    <option name="y" value="23" />
+    <option name="width" value="1280" />
+    <option name="height" value="777" />
+  </component>
+  <component name="ProjectView">
+    <navigator currentView="ProjectPane" proportions="" version="1">
+      <flattenPackages />
+      <showMembers />
+      <showModules />
+      <showLibraryContents />
+      <hideEmptyPackages />
+      <abbreviatePackageNames />
+      <autoscrollToSource />
+      <autoscrollFromSource />
+      <sortByType />
+      <manualOrder />
+      <foldersAlwaysOnTop value="true" />
+    </navigator>
+    <panes>
+      <pane id="Scratches" />
+      <pane id="ProjectPane">
+        <subPane>
+          <expand>
+            <path>
+              <item name="analysis" type="b2602c69:ProjectViewProjectNode" />
+              <item name="analysis" type="462c0819:PsiDirectoryNode" />
+            </path>
+          </expand>
+          <select />
+        </subPane>
+      </pane>
+      <pane id="Scope" />
+    </panes>
+  </component>
+  <component name="PropertiesComponent">
+    <property name="settings.editor.selected.configurable" value="reference.settingsdialog.IDE.editor.colors" />
+    <property name="nodejs_interpreter_path.stuck_in_default_project" value="undefined stuck path" />
+    <property name="last_opened_file_path" value="$USER_HOME$" />
+    <property name="DefaultGoTemplateProperty" value="Go Application" />
+  </component>
+  <component name="RunDashboard">
+    <option name="ruleStates">
+      <list>
+        <RuleState>
+          <option name="name" value="ConfigurationTypeDashboardGroupingRule" />
+        </RuleState>
+        <RuleState>
+          <option name="name" value="StatusDashboardGroupingRule" />
+        </RuleState>
+      </list>
+    </option>
+  </component>
+  <component name="RunManager">
+    <configuration name="go build analysis.go" type="GoApplicationRunConfiguration" factoryName="Go Application" temporary="true">
+      <module name="analysis" />
+      <working_directory value="$PROJECT_DIR$/" />
+      <go_parameters value="-i" />
+      <kind value="FILE" />
+      <filePath value="$PROJECT_DIR$/analysis.go" />
+      <package value="analysis" />
+      <directory value="$PROJECT_DIR$/" />
+    </configuration>
+    <recent_temporary>
+      <list size="1">
+        <item index="0" class="java.lang.String" itemvalue="Go Build.go build analysis.go" />
+      </list>
+    </recent_temporary>
+  </component>
+  <component name="ShelveChangesManager" show_recycled="false">
+    <option name="remove_strategy" value="false" />
+  </component>
+  <component name="ToolWindowManager">
+    <frame x="0" y="23" width="1280" height="777" extended-state="6" />
+    <editor active="true" />
+    <layout>
+      <window_info id="Project" active="false" anchor="left" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="true" show_stripe_button="true" weight="0.20597738" sideWeight="0.5" order="0" side_tool="false" content_ui="combo" />
+      <window_info id="TODO" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.33" sideWeight="0.5" order="6" side_tool="false" content_ui="tabs" />
+      <window_info id="Event Log" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.7394636" sideWeight="0.5040733" order="7" side_tool="true" content_ui="tabs" />
+      <window_info id="Database" active="false" anchor="right" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.33" sideWeight="0.5" order="3" side_tool="false" content_ui="tabs" />
+      <window_info id="Run" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.4597701" sideWeight="0.49592668" order="2" side_tool="false" content_ui="tabs" />
+      <window_info id="Version Control" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="false" weight="0.33" sideWeight="0.5" order="7" side_tool="false" content_ui="tabs" />
+      <window_info id="Structure" active="false" anchor="left" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.25" sideWeight="0.5" order="1" side_tool="false" content_ui="tabs" />
+      <window_info id="Terminal" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.12452107" sideWeight="0.49592668" order="7" side_tool="false" content_ui="tabs" />
+      <window_info id="Debug" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.3991684" sideWeight="0.5" order="3" side_tool="false" content_ui="tabs" />
+      <window_info id="Favorites" active="false" anchor="left" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.33" sideWeight="0.5" order="2" side_tool="true" content_ui="tabs" />
+      <window_info id="Cvs" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.25" sideWeight="0.5" order="4" side_tool="false" content_ui="tabs" />
+      <window_info id="Message" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.33" sideWeight="0.5" order="0" side_tool="false" content_ui="tabs" />
+      <window_info id="Commander" active="false" anchor="right" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.4" sideWeight="0.5" order="0" side_tool="false" content_ui="tabs" />
+      <window_info id="Inspection" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.4" sideWeight="0.5" order="5" side_tool="false" content_ui="tabs" />
+      <window_info id="Hierarchy" active="false" anchor="right" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.25" sideWeight="0.5" order="2" side_tool="false" content_ui="combo" />
+      <window_info id="Find" active="false" anchor="bottom" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.33" sideWeight="0.5" order="1" side_tool="false" content_ui="tabs" />
+      <window_info id="Ant Build" active="false" anchor="right" auto_hide="false" internal_type="DOCKED" type="DOCKED" visible="false" show_stripe_button="true" weight="0.25" sideWeight="0.5" order="1" side_tool="false" content_ui="tabs" />
+    </layout>
+  </component>
+  <component name="TypeScriptGeneratedFilesManager">
+    <option name="version" value="1" />
+  </component>
+  <component name="VcsContentAnnotationSettings">
+    <option name="myLimit" value="2678400000" />
+  </component>
+  <component name="XDebuggerManager">
+    <breakpoint-manager />
+    <watches-manager />
+  </component>
+  <component name="editorHistoryManager">
+    <entry file="file:///usr/local/Cellar/go/1.9.2/libexec/src/bytes/buffer.go">
+      <provider selected="true" editor-type-id="text-editor">
+        <state relative-caret-position="56">
+          <caret line="90" column="0" lean-forward="false" selection-start-line="90" selection-start-column="0" selection-end-line="90" selection-end-column="0" />
+        </state>
+      </provider>
+    </entry>
+    <entry file="file://$PROJECT_DIR$/../github.com/mediocregopher/radix.v2/redis/client.go">
+      <provider selected="true" editor-type-id="text-editor">
+        <state relative-caret-position="143">
+          <caret line="190" column="0" lean-forward="false" selection-start-line="190" selection-start-column="0" selection-end-line="190" selection-end-column="0" />
+        </state>
+      </provider>
+    </entry>
+    <entry file="file://$PROJECT_DIR$/analysis.go">
+      <provider selected="true" editor-type-id="text-editor">
+        <state relative-caret-position="-5594">
+          <caret line="5" column="10" lean-forward="false" selection-start-line="5" selection-start-column="10" selection-end-line="5" selection-end-column="10" />
+          <folding />
+        </state>
+      </provider>
+    </entry>
+  </component>
+</project>package main
 
 import (
 	"flag"
-	"os"
-	"time"
-
 	"github.com/sirupsen/logrus"
+	"time"
+	"os"
+	"bufio"
+	"io"
+	"strings"
+	"github.com/mgutz/str"
+	"net/url"
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/mediocregopher/radix.v2/pool"
+	"strconv"
 )
+
+const HANDLE_DIG = " /dig?"
+const HANDLE_MOVIE = "/movie/"
+const HANDLE_LIST = "/list/"
+const HANDLE_HTML = ".html"
+
 
 type cmdParams struct {
 	logFilePath string
-	routineNum  int
+	routineNum int
 }
-
-type digData struct {
-	time  string
-	url   string
-	refer string
-	ua    string
+type digData struct{
+	time   string
+	url    string
+	refer  string
+	ua        string
 }
-
 type urlData struct {
-	data digData
-	uid  string //用户id
+	data   digData
+	uid    string
+	unode  urlNode
 }
-
 type urlNode struct {
+	unType     string // 详情页 或者 列表页 或者 首页
+	unRid  int       // Resource ID 资源ID
+	unUrl  string // 当前这个页面的url
+	unTime  string // 当前访问这个页面的时间
 }
-
 type storageBlock struct {
-	counterType  string
-	storageModel string
+	counterType       string
+	storageModel   string
 	unode        urlNode
 }
 
@@ -38,73 +234,230 @@ var log = logrus.New()
 
 func init() {
 	log.Out = os.Stdout
-	log.SetLevel(logrus.DebugLevel)
+	log.SetLevel( logrus.DebugLevel )
 }
 
 func main() {
-	//获取参数
-	logFilePath := flag.String("logFilePath", "nginx/.log", "--0")
-	routineNum := flag.Int("routineNum", 5, "---")
-	l := flag.String("l", "tmp/log", "---")
+	// 获取参数
+	logFilePath := flag.String( "logFilePath", "/Users/pangee/Public/nginx/logs/dig.log", "log file path" )
+	routineNum := flag.Int( "routineNum", 5, "consumer numble by goroutine" )
+	l := flag.String( "l", "/tmp/log", "this programe runtime log target file path" )
 	flag.Parse()
 
-	params := cmdParams{
-		*logFilePath,
-		*routineNum,
-	}
+	params := cmdParams{ *logFilePath, *routineNum }
 
-	//打日志
-	logFd, err := os.OpenFile(*l, os.O_APPEND|os.O_WRONLY, 0644)
+	// 打日志
+	logFd, err := os.OpenFile( *l, os.O_CREATE|os.O_WRONLY, 0644 )
 	if err == nil {
 		log.Out = logFd
 		defer logFd.Close()
 	}
-	log.Infof("Exec start.")
-	log.Infof("logFilePath = %s,routneNum = %d", logFilePath, routineNum)
+	log.Infof( "Exec start." )
+	log.Infof( "Params: logFilePath=%s, routineNum=%d", params.logFilePath, params.routineNum )
 
-	//初始化一些channel，用于数据传递
-	var (
-		logChannel     = make(chan string, 3*params.routineNum)
-		pvChannel      = make(chan urlData, params.routineNum)
-		uvChannel      = make(chan urlData, params.routineNum)
-		storageChannel = make(chan storageBlock, params.routineNum)
-	)
+	// 初始化一些channel，用于数据传递
+	var logChannel = make(chan string, 3*params.routineNum)
+	var pvChannel = make(chan urlData, params.routineNum)
+	var uvChannel = make(chan urlData, params.routineNum)
+	var storageChannel = make(chan storageBlock, params.routineNum)
 
-	//创建日志消费者
-	go readFileLinebyLine(params, logChannel)
-
-	//创建一组日志处理
-	for i := 0; i < params.routineNum; i++ {
-		go logConsumer(logChannel, pvChannel, uvChannel)
+	// Redis Pool
+	redisPool, err := pool.New( "tcp", "localhost:6379", 2*params.routineNum );
+	if err != nil{
+		log.Fatalln( "Redis pool created failed." )
+		panic(err)
+	} else {
+		go func(){
+			for{
+				redisPool.Cmd( "PING" )
+				time.Sleep( 3*time.Second )
+			}
+		}()
 	}
 
-	//创建PV UV统计器
-	go pvCounter(pvChannel, storageChannel)
-	go uvCounter(uvChannel, storageChannel)
-	//可以继续扩展的 xxCounter
 
-	//创建存储器
-	go dataStorage(storageChannel)
+	// 日志消费者
+	go readFileLinebyLine( params, logChannel )
 
-	time.Sleep(1000 * time.Second)
+	// 创建一组日志处理
+	for i:=0; i<params.routineNum; i++ {
+		go logConsumer( logChannel, pvChannel, uvChannel )
+	}
+
+	// 创建PV UV 统计器
+	go pvCounter( pvChannel, storageChannel )
+	go uvCounter( uvChannel, storageChannel, redisPool )
+	// 可扩展的 xxxCounter
+
+	// 创建 存储器
+	go dataStorage( storageChannel, redisPool )
+
+	time.Sleep( 1000*time.Second )
 }
 
-func readFileLinebyLine(params cmdParams, logChannel chan string) {
+// HBase 劣势：列簇需要声明清楚
+func dataStorage( storageChannel chan storageBlock, redisPool *pool.Pool) {
+	for block := range storageChannel {
+		prefix := block.counterType + "_"
 
+		// 逐层添加，加洋葱皮的过程
+		// 维度： 天-小时-分钟
+		// 层级： 定级-大分类-小分类-终极页面
+		// 存储模型： Redis  SortedSet
+		setKeys := []string{
+			prefix+"day_"+getTime(block.unode.unTime, "day"),
+			prefix+"hour_"+getTime(block.unode.unTime, "hour"),
+			prefix+"min_"+getTime(block.unode.unTime, "min"),
+			prefix+block.unode.unType+"_day_"+getTime(block.unode.unTime, "day"),
+			prefix+block.unode.unType+"_hour_"+getTime(block.unode.unTime, "hour"),
+			prefix+block.unode.unType+"_min_"+getTime(block.unode.unTime, "min"),
+		}
+
+		rowId := block.unode.unRid
+
+		for _,key := range setKeys {
+			ret, err := redisPool.Cmd( block.storageModel, key, 1, rowId ).Int()
+			if ret<=0 || err!=nil {
+				log.Errorln( "DataStorage redis storage error.", block.storageModel, key, rowId )
+			}
+		}
+	}
 }
 
-func logConsumer(logChannel chan string, pvChannel, uvChannel chan urlData) {
+func pvCounter( pvChannel chan urlData, storageChannel chan storageBlock ) {
+	for data := range pvChannel {
+		sItem := storageBlock{ "pv", "ZINCRBY", data.unode }
+		storageChannel <- sItem
+	}
+}
+func uvCounter( uvChannel chan urlData, storageChannel chan storageBlock, redisPool *pool.Pool ) {
+	for data := range uvChannel {
+		//HyperLoglog redis
+		hyperLogLogKey := "uv_hpll_"+getTime(data.data.time, "day")
+		ret, err := redisPool.Cmd( "PFADD", hyperLogLogKey, data.uid, "EX", 86400 ).Int()
+		if err!=nil {
+			log.Warningln( "UvCounter check redis hyperloglog failed, ", err )
+		}
+		if ret!=1 {
+			continue
+		}
 
+		sItem := storageBlock{ "uv", "ZINCRBY", data.unode }
+		storageChannel <- sItem
+	}
 }
 
-func pvCounter(pvChannel chan urlData, storageChannel chan storageBlock) {
+func logConsumer( logChannel chan string, pvChannel, uvChannel chan urlData ) error {
+	for logStr := range logChannel {
+		// 切割日志字符串，扣出打点上报的数据
+		data := cutLogFetchData( logStr )
 
+		// uid
+		// 说明： 课程中模拟生成uid， md5(refer+ua)
+		hasher := md5.New()
+		hasher.Write( []byte( data.refer+data.ua ) )
+		uid := hex.EncodeToString( hasher.Sum(nil) )
+
+		// 很多解析的工作都可以放到这里完成
+		// ...
+		// ...
+
+		uData := urlData{ data, uid, formatUrl( data.url, data.time ) }
+
+		pvChannel <- uData
+		uvChannel <- uData
+	}
+	return nil
+}
+func cutLogFetchData( logStr string ) digData {
+	logStr = strings.TrimSpace( logStr )
+	pos1 := str.IndexOf( logStr,  HANDLE_DIG, 0)
+	if pos1==-1 {
+		return digData{}
+	}
+	pos1 += len( HANDLE_DIG )
+	pos2 := str.IndexOf( logStr, " HTTP/", pos1 )
+	d := str.Substr( logStr, pos1, pos2-pos1 )
+
+	urlInfo, err := url.Parse( "http://localhost/?"+d )
+	if err != nil {
+		return digData{}
+	}
+	data := urlInfo.Query()
+	return digData{
+		data.Get("time"),
+		data.Get("refer"),
+		data.Get("url"),
+		data.Get("ua"),
+	}
+}
+func readFileLinebyLine( params cmdParams, logChannel chan string ) error {
+	fd, err := os.Open( params.logFilePath )
+	if err != nil {
+		log.Warningf( "ReadFileLinebyLine can't open file:%s", params.logFilePath )
+		return err
+	}
+	defer fd.Close()
+
+	count := 0
+	bufferRead := bufio.NewReader( fd )
+	for {
+		line, err := bufferRead.ReadString( '\n' )
+		logChannel <- line
+		count++
+
+		if count%(1000*params.routineNum) == 0 {
+			log.Infof( "ReadFileLinebyLine line: %d", count )
+		}
+		if err != nil {
+			if err == io.EOF {
+				time.Sleep( 3*time.Second )
+				log.Infof( "ReadFileLinebyLine wait, raedline:%d", count )
+			} else {
+				log.Warningf( "ReadFileLinebyLine read log error" )
+			}
+		}
+	}
+	return nil
 }
 
-func uvCounter(uvChannel chan urlData, storageChannel chan storageBlock) {
 
+func formatUrl( url, t string ) urlNode{
+	// 一定从量大的着手,  详情页>列表页≥首页
+	pos1 := str.IndexOf( url, HANDLE_MOVIE, 0)
+	if pos1!=-1 {
+		pos1 += len( HANDLE_MOVIE )
+		pos2 := str.IndexOf( url, HANDLE_HTML, 0 )
+		idStr := str.Substr( url , pos1, pos2-pos1 )
+		id, _ := strconv.Atoi( idStr )
+		return urlNode{ "movie", id, url, t }
+	} else {
+		pos1 = str.IndexOf( url, HANDLE_LIST, 0 )
+		if pos1!=-1 {
+			pos1 += len( HANDLE_LIST )
+			pos2 := str.IndexOf( url, HANDLE_HTML, 0 )
+			idStr := str.Substr( url , pos1, pos2-pos1 )
+			id, _ := strconv.Atoi( idStr )
+			return urlNode{ "list", id, url, t }
+		} else {
+			return urlNode{ "home", 1, url, t}
+		} // 如果页面url有很多种，就不断在这里扩展
+	}
 }
 
-func dataStorage(storageChannel chan storageBlock) {
-
+func getTime( logTime, timeType string ) string {
+	var item string
+	switch timeType {
+	case "day":
+		item = "2006-01-02"
+		break
+	case "hour":
+		item = "2006-01-02 15"
+		break
+	case "min":
+		item = "2006-01-02 15:04"
+		break
+	}
+	t, _ := time.Parse( item, time.Now().Format(item) )
+	return strconv.FormatInt( t.Unix(), 10 )
 }
